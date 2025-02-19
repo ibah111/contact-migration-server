2c778191bf2d50ef8be25acaa8e190c03ab10206
ibah <starlord77@mail.ru>
2025-02-19 12:17:07 +0300

    feat: sqlite db setup

---
+++ src/modules/database/sqlite/sqlite.database.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [],
+  providers: [],
+})
+export default class SqliteDatabaseModule {}
