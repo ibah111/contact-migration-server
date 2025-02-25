a9c29ee568b3c455a9fd124f23098792434dc3ab
ibah <starlord77@mail.ru>
2025-02-25 11:45:17 +0300

    feat: solved the config problem

--- src/modules/smb/smb.service.ts
+++ src/modules/smb/smb.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
 import { node } from 'src/main';
 const SMB2 = require('smb2');
 
@@ -12,29 +13,32 @@ export default class SmbService {
   private username: string;
   private password: string;
   constructor(private readonly config: ConfigService) {
-    this.share = this.config.get<string>('share')!;
+    this.share = this.config.get<string>('share')!.replace(/\\\\/g, '\\'); // <=== Solved problem
     this.domain = this.config.get<string>('domain')!;
     this.username = this.config.get<string>('smb_username')!;
     this.password = this.config.get<string>('smb_password')!;
+    const right_share = this.share === '\\\\newct.usb.ru\\Luxbase';
+    const right_domain = this.domain === 'usb.ru';
     this.smb = new SMB2({
-      share: '\\\\newct.usb.ru\\Luxbase\\',
+      share: '\\\\newct.usb.ru\\Luxbase',
       domain: 'usb.ru',
-      username: 'Contact-SMB',
-      password: 'Contact-SMB',
+      username: this.username,
+      password: this.password,
       port: 445,
     });
     console.log(
-      'smb'.yellow,
-      this.share,
-      this.domain,
-      this.username,
-      this.password,
+      this.share.blue,
+      '\\\\newct.usb.ru\\Luxbase'.yellow,
+      '\n',
+      'right_share:',
+      right_share ? 'Share correct'.green : 'Share wrong'.red,
+      '\n',
+      'right_domain:',
+      right_domain ? 'Domain correct'.green : 'Domain wrong'.red,
     );
   }
 
-  //"\\newct\luxbase\Docattach\0\0DD92B61-6775-4B51-8233-3CE4BD6E4656..pdf"
   async exists(path: string) {
-    console.log(path);
     return new Promise((resolve, reject) => {
       this.smb.exists(path, (err: any, exists: any) => {
         if (err) reject(err);
@@ -44,15 +48,10 @@ export default class SmbService {
   }
 
   async readdir(path: string) {
-    node ?? console.log('readdir path'.yellow, path);
     return new Promise((resolve, reject) => {
       this.smb.readdir(`Docattach\\${path}`, (err: any, files: string[]) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(files);
-          console.log(files);
-        }
+        if (err) reject(err);
+        resolve(files);
       });
     });
   }
