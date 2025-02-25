df465379a32847447ffc2bf8f8556444e7b0a80d
ibah <starlord77@mail.ru>
2025-02-25 14:50:09 +0300

    feat: api description

--- src/pages/smb/smb.page.controller.ts
+++ src/pages/smb/smb.page.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import PortfolioService from './smb.page.service';
 import { SMBPathInput } from './smb.input';
 
@@ -8,16 +8,25 @@ import { SMBPathInput } from './smb.input';
 export default class SmbPageController {
   constructor(private readonly service: PortfolioService) {}
 
+  @ApiOperation({
+    description: 'Чтение директории от папки Docattach',
+  })
   @Post('readdir')
   async readdir(@Body() { path }: SMBPathInput) {
     return await this.service.readdir(path);
   }
 
+  @ApiOperation({
+    description: 'Проверка файла на существование',
+  })
   @Post('exists')
   async exists(@Body() { path }: SMBPathInput) {
     return await this.service.exists(path);
   }
 
+  @ApiOperation({
+    description: 'Чтение файла. Возвращается buffer',
+  })
   @Post('readFile')
   async readFile(@Body('path') { path }: SMBPathInput) {
     return await this.service.readFile(path);
