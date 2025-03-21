0bd1ca80f5f4c097a1f64f06cf49b88707a4a442
ibah <starlord77@mail.ru>
2025-03-21 14:56:47 +0300

    feat: sqlite module to common pool DB module

--- src/modules/database/database.modules.ts
+++ src/modules/database/database.modules.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import ContactDatabase from './contact.database';
+import SqliteDatabaseModule from './sqlite/sqlite.database.module';
 
 @Module({
-  imports: [ContactDatabase],
+  imports: [ContactDatabase, SqliteDatabaseModule],
 })
 export default class DatabaseModule {}
