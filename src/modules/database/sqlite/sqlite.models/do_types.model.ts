1fdcf297fc33df9d2ad0f78df1d4b1d8ffaa6435
ibah <starlord77@mail.ru>
2025-03-21 17:04:16 +0300

    feat: model building

--- src/modules/database/sqlite/sqlite.models/do_types.model.ts
+++ src/modules/database/sqlite/sqlite.models/do_types.model.ts
@@ -1,6 +1,9 @@
-import { Model, Table } from 'sequelize-typescript';
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
 
 @Table({
   tableName: 'do_types',
 })
-export default class DO_Types extends Model {}
+export default class DO_Types extends Model {
+  @Column(DataType.STRING)
+  name: string;
+}
