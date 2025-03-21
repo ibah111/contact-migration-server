6efe88fb84fa0cd01b634634cf53fe5fe94d5906
ibah <starlord77@mail.ru>
2025-03-21 14:56:26 +0300

    feat: seed and connection in sqlite module

--- src/modules/database/sqlite/sqlite.database.module.ts
+++ src/modules/database/sqlite/sqlite.database.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { models } from './sqlite.models';
+import { SqliteDatabaseSeed } from './sqlite.seed';
 
 @Module({
-  imports: [],
-  providers: [],
+  imports: [
+    SequelizeModule.forRoot({
+      logging: false,
+      name: 'sqlite',
+      dialect: 'sqlite',
+      storage: 'database.sqlite',
+      models,
+    }),
+  ],
+  providers: [SqliteDatabaseSeed],
 })
 export default class SqliteDatabaseModule {}
