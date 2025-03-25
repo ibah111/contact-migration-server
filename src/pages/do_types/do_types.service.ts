e91714c89e25c2662d11462d7a0d0781ef66ecff
ibah <starlord77@mail.ru>
2025-03-25 20:17:52 +0300

    feat: service + module

---
+++ src/pages/do_types/do_types.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import DO_Types from 'src/modules/database/sqlite/sqlite.models/do_types.model';
+import { DoTypesInput } from './do_types.input';
+import { Op } from 'sequelize';
+
+@Injectable()
+export default class DO_TypesService {
+  constructor(
+    @InjectModel(DO_Types, 'sqlite')
+    private readonly modelDoType: typeof DO_Types,
+  ) {}
+
+  async createDoTypes({ name }: DoTypesInput) {
+    return await this.modelDoType.create({
+      name,
+    });
+  }
+
+  async readDoTypes({ name }: DoTypesInput) {
+    return await this.modelDoType.findAll({
+      where: {
+        name: {
+          [Op.like]: `%${name}%`,
+        },
+      },
+    });
+  }
+
+  async updateDoTypes({ id, name }: DoTypesInput) {
+    return await this.modelDoType.update(
+      {
+        name,
+      },
+      {
+        where: {
+          id,
+        },
+      },
+    );
+  }
+
+  async deleteDoTypes({ id }) {
+    return await this.modelDoType.destroy({
+      where: {
+        id,
+      },
+    });
+  }
+}
--- src/pages/pages.module.ts
+++ src/pages/pages.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SmbPageModule } from './smb/smb.page.module';
+import DO_TypesModule from './do_types/do_types.module';
 
 @Module({
-  imports: [SmbPageModule],
+  imports: [SmbPageModule, DO_TypesModule],
 })
 export default class PagesModule {}
