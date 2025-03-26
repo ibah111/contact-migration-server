956c37686a7eb12a9d3d5cf4864efd295eff58d6
ibah <starlord77@mail.ru>
2025-03-26 12:46:54 +0300

    feat(marsaud/sm2): updated package

--- src/modules/smb/smb.service.ts
+++ src/modules/smb/smb.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
 import { node } from 'src/main';
-const SMB2 = require('smb2');
+const SMB2 = require('@marsaud/smb2');
 
 @Injectable()
 export default class SmbService {
