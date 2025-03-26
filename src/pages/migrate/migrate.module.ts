d07be290e45447119e273d38db5309941b26d81c
ibah <starlord77@mail.ru>
2025-03-26 15:34:38 +0300

    feat: Ftp module imported to migrate page

--- src/pages/migrate/migrate.module.ts
+++ src/pages/migrate/migrate.module.ts
@@ -5,12 +5,14 @@ import DO_Types from 'src/modules/database/sqlite/sqlite.models/do_types.model';
 import MigrateController from './migrate.controller';
 import MigrateService from './migrate.service';
 import SmbModule from 'src/modules/smb/smb.module';
+import FTPModule from 'src/modules/ftp/ftp.module';
 
 @Module({
   imports: [
     SequelizeModule.forFeature([DocAttach, Portfolio, Debt], 'contact'),
     SequelizeModule.forFeature([DO_Types], 'sqlite'),
     SmbModule,
+    FTPModule,
   ],
   controllers: [MigrateController],
   providers: [MigrateService],
