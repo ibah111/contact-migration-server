6468c8d8d0fd4872b6cfce9b0801a4a2a1789024
ibah <starlord77@mail.ru>
2025-04-10 00:10:13 +0300

    feat: dev default data input

--- src/pages/migrate/migrate.input.ts
+++ src/pages/migrate/migrate.input.ts
@@ -1,23 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { node } from 'src/main';
 
 export class MigrateInput {
   @ApiProperty({
     description: 'Тип документа',
-    default: 'запрос',
+    default: node === 'dev' ? 'запрос' : null,
   })
   do_type_name: string;
   @ApiProperty({
     description: 'id портфеля',
-    default: 88,
+    default: node === 'dev' ? 88 : null,
   })
   r_portfolio_id: number;
   @ApiProperty({
     description: 'Тип вложения: 1 - судебная работа, 2 - испол. пр-во',
-    default: 1,
+    default: node === 'dev' ? 1 : null,
   })
   include_type: 1 | 2;
   @ApiProperty({
-    default: true,
+    default: node === 'dev' ? true : null,
   })
   upload: boolean;
 }
